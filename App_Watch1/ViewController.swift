b3b770304b55435255d2c15ec04f9651cb7c24ca
cice <cice@aula8PovMacN09.local>
2017-04-07 17:19:04 +0200

    Initial Commit

---
+++ App_Watch1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_Watch1
+//
+//  Created by cice on 7/4/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
